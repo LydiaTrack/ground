a341b5a75944b496a6acf0bcb6174e0a3acbf523
Muratcan Şentürk <muratcan.senturk@ozguryazilim.com.tr>
2023-07-04 20:27:41 +0300

    feat: Authentication
    * Added default user initializer
    
    Refs: LT-7

---
+++ internal/utils/default_user_initializer.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"lydia-track-base/internal/domain"
+	"lydia-track-base/internal/domain/commands"
+	"lydia-track-base/internal/repository"
+	"lydia-track-base/internal/service"
+	"time"
+)
+
+// InitializeDefaultUser initializes the default user with default credentials
+func InitializeDefaultUser() error {
+	userCreateCmd := commands.CreateUserCommand{
+		Username: "lydia",
+		Password: "P@ssw0rd",
+		PersonInfo: domain.PersonInfo{
+			FirstName: "Lydia",
+			LastName:  "Admin",
+			BirthDate: primitive.
+				DateTime(time.Date(1990, 1, 1, 0, 0, 0, 0, time.UTC).UnixNano() / int64(time.Millisecond)),
+		},
+	}
+
+	_, err := service.
+		NewUserService(repository.GetRepository()).
+		CreateUser(userCreateCmd)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
