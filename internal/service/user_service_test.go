f5c3d83f17d2e0dce3e648c134ae7ca25f35d4b7
snturk <mcsnturk@gmail.com>
2023-10-01 14:13:50 +0300

    fix: Test Optimizations
    * Optimized tests with applying changes to `test_mongo.go`
    
    Refs: #17

--- internal/service/user_service_test.go
+++ internal/service/user_service_test.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"golang.org/x/crypto/bcrypt"
 	"lydia-track-base/internal/domain/user"
 	"lydia-track-base/internal/domain/user/commands"
 	"lydia-track-base/internal/repository"
@@ -13,9 +12,7 @@ import (
 
 // TestNewUserService Create a new user service instance with UserMongoRepository
 func TestNewUserService(t *testing.T) {
-	// Initialize mongo test support
-	test_support.TestWithMongo(t)
-
+	test_support.TestWithMongo()
 	repo := repository.GetUserRepository()
 
 	// Create a new user service instance
@@ -24,16 +21,13 @@ func TestNewUserService(t *testing.T) {
 
 // TestCreateUser Create a new user
 func TestCreateUser(t *testing.T) {
-	// Initialize mongo test support
-	test_support.TestWithMongo(t)
+	test_support.TestWithMongo()
 
-	repo := repository.GetUserRepository()
-
-	// Create a new user service instance
-	userService := NewUserService(repo)
+	// Create a new userModel service instance
+	userService := NewUserService(repository.GetUserRepository())
 
 	birthDate := primitive.NewDateTimeFromTime(time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC))
-	// Create a new user
+	// Create a new userModel
 	command := commands.CreateUserCommand{
 		Username: "test",
 		Password: "test123",
@@ -50,51 +44,46 @@ func TestCreateUser(t *testing.T) {
 			},
 		},
 	}
-	user, err := userService.CreateUser(command)
+	userModel, err := userService.CreateUser(command)
 
 	if err != nil {
-		t.Errorf("Error creating user test: %v", err)
+		t.Errorf("Error creating userModel test: %v", err)
 	} else {
 
-		if user.Username != "test" {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.Username != "test" {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 
-		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte("test123"))
-		if err != nil {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.PersonInfo.FirstName != "TestName" {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 
-		if user.PersonInfo.FirstName != "TestName" {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.PersonInfo.LastName != "Test Lastname" {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 
-		if user.PersonInfo.LastName != "Test Lastname" {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.PersonInfo.Email != "exampletest@example.com" {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 
-		if user.PersonInfo.Email != "exampletest@example.com" {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.PersonInfo.Address != "Test Address" {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 
-		if user.PersonInfo.Address != "Test Address" {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.PersonInfo.BirthDate != birthDate {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 
-		if user.PersonInfo.BirthDate != birthDate {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.PersonInfo.PhoneNumber.AreaCode != "500" {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 
-		if user.PersonInfo.PhoneNumber.AreaCode != "500" {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.PersonInfo.PhoneNumber.Number != "5005050" {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 
-		if user.PersonInfo.PhoneNumber.Number != "5005050" {
-			t.Errorf("Error creating user: %v", err)
-		}
-
-		if user.PersonInfo.PhoneNumber.CountryCode != "+90" {
-			t.Errorf("Error creating user: %v", err)
+		if userModel.PersonInfo.PhoneNumber.CountryCode != "+90" {
+			t.Errorf("Error creating userModel: %v", err)
 		}
 	}
 }
