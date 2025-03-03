a433a7b41d3d7f2b58cad304e4ecac375d82f2f2
snturk <mcsnturk@gmail.com>
2025-03-03 21:17:47 +0300

    added multiple env file support

--- pkg/test_support/test_mongo.go
+++ pkg/test_support/test_mongo.go
@@ -4,13 +4,23 @@ import (
 	"github.com/LydiaTrack/ground/pkg/log"
 	"github.com/LydiaTrack/ground/pkg/mongodb"
 	"github.com/joho/godotenv"
+	"os"
 )
 
 func TestWithMongo() {
 	// Initialize logging
 	log.InitLogging()
 	// Initialize environment variables
-	err := godotenv.Load()
+	fileName := ""
+	envType := os.Getenv("ENV_TYPE")
+	if envType == "production" {
+		fileName = ".env.prod"
+	} else if envType == "test" {
+		fileName = ".env.test"
+	} else {
+		fileName = ".env.development"
+	}
+	err := godotenv.Load(fileName)
 	if err != nil {
 		log.LogFatal("Error loading .env file")
 	}
